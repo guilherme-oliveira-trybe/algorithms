dda812a675e4f63634d454da3c8588478d0ac4ce
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-01-25 16:43:22 -0300

    feat: def study_schedule responsável por informar o número de estudantes no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    students_quantity = 0
+
+    for entry, exit in permanence_period:
+        if entry is None or exit is None or target_time is None:
+            return None
+
+        if type(entry) is str or type(exit) is str:
+            return None
+
+        if int(entry) <= target_time <= int(exit):
+            students_quantity += 1
+
+    return students_quantity
