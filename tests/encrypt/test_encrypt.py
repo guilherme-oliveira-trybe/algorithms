fa3be578b6170b038d5c0024f5e49a2d9aea7282
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-01-25 17:07:43 -0300

    test(test_encrypt_message): Verifica se a função encrypt_message respeita uma lógica específica

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(110, 4)
+
+    with pytest.raises(TypeError):
+        encrypt_message("guilherme", "teste")
+
+    assert encrypt_message("guilherme", 10) == "emrehliug"
+    assert encrypt_message("guilherme", 3) == "iug_emrehl"
+    assert encrypt_message("guilherme", 4) == "emreh_liug"
