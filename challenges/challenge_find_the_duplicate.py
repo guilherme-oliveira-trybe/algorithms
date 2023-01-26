1319660edda6f1c31765242f7e7ad791d87985b7
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-01-26 15:00:50 -0300

    feat: def find_duplicate verifica se existe números repetidos dentro de um array

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+
+    for index in range(1, len(nums)):
+        if type(nums[index]) is str:
+            return False
+        elif len(nums) < 2 or nums[index] < 0:
+            return False
+        elif nums[index] == nums[index - 1]:
+            return nums[index]
+
+    return False
